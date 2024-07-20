c13f772ec931e25527cb3fd0a73726438105b2f7
roihezkiyahu <rhezki11@gmail.com>
2024-07-20 11:55:19 +0300

    readme update new configs for space invaders

--- Atari_runner.py
+++ Atari_runner.py
@@ -248,9 +248,9 @@ if __name__ == "__main__":
     # config_path = os.path.join("modeling", "configs", "trainer_config_SpaceInvaders.yaml")
     # train_agent(config_path, None, None)
 
-    # config_path = os.path.join("modeling", "configs", "trainer_config_SpaceInvaders_llp50.yaml")
-    # train_agent(config_path, None, None)
-    #
+    config_path = os.path.join("modeling", "configs", "trainer_config_SpaceInvaders_llp50_epsdcay2k.yaml")
+    train_agent(config_path, None, None)
+
     # config_path = os.path.join("modeling", "configs", "trainer_config_SpaceInvaders_llp250.yaml")
     # train_agent(config_path, None, None)
 
